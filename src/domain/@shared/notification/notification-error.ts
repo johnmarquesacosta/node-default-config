394ece016a744d1cdcd663496ed50a51711fa666
John Marques <johnmarquesacosta@gmail.com>
2024-03-31 18:18:41 -0300

    feat: first commit

--- src/domain/@shared/notification/notification-error.ts
+++
@@ -1,7 +0,0 @@
-import { NotificationErrorProps } from "./notification"
-
-export default class NotificationError extends Error {
-  constructor(readonly errors: NotificationErrorProps[]) {
-    super(errors.map((error) => error.message).join(", "))
-  }
-}
--- src/domain/@shared/validator/validator.interface.ts
+++
@@ -1,3 +0,0 @@
-export default interface IValidator<T> {
-  validate(entity: T): boolean
-}
